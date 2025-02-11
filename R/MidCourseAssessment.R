db2bc229a881d50b8dddba77d26f18b937ca6665
vlwbirds <vlwbirds@gmail.com>
2025-02-11 13:33:56 -0800

    seal regression

--- R/MidCourseAssessment.R
+++ R/MidCourseAssessment.R
@@ -135,16 +135,16 @@ m1 <- lm(RSV_gc_g_dry_weight ~ Influenza_A_gc_g_dry_weight, data = ww); summary(
 #   
 #   I. What is the slope of the relationship between RNA of Influenza and RNA of RSV? 
 
-# slope = 5.113e-01
+#        slope = 5.113e-01
 
 #      What is the uncertainty in the slope? 
 
-# uncertainty = 3.603e-02 = SE
+#        uncertainty = 3.603e-02 = SE
 #       
 #      What is the approximate 95% CI of the slope? 
 5.113e-01 + (2 * 3.603e-02) # 0.58336
 5.113e-01 - (2 * 3.603e-02) # 0.43924
-confint(m1, level = 0.95) # Influenza_A_gc_g_dry_weight    0.44046    0.5821131
+confint(m1, level = 0.95) # Influenza_A_gc_g_dry_weight    0.44046    0.5821131, I like this way better because there's less likelihood for a simple math error.
 
 #   
 #   J. If you wanted to have a more precise measurement of the slope, what are two things you could do to make the uncertainty in the slope smaller?
@@ -168,21 +168,98 @@ confint(m1, level = 0.95) # Influenza_A_gc_g_dry_weight    0.44046    0.5821131
 # 
 # If you are using base plot and want to add the regression equation and R2, see the Part 2: Regression slides which show how to do this by extracting and plotting model components.
 # 
-# M. Load in the dataset from the last lecture (NSFRoD2022_Deployment_Summary_RSB.csv). Let’s repeat the exercise that we did in class – to see if size increases with age and sex in elephant seals – but this time, use Standard Length (cm) instead of mass. Run an additive multiple regression of Standard Length with Age and Sex as predictors. Then, run an interactive multiple regression of Standard Length with Age and Sex as predictors. Write R code that produces/prints the intercepts and slopes of each model (feel free to either fill in a single number, or include multiple numbers in each cell to show us how you calculated e.g., intercept for males in the interactive model). 
+library(ggpubr)
+
+rsv_inf_R2 <- ggplot(ww, aes(Influenza_A_gc_g_dry_weight, RSV_gc_g_dry_weight)) +
+  geom_point(color = "blue", size = 3, alpha = 0.7) + 
+  geom_smooth(method = "lm", color = "red", fill = "pink", se = TRUE, size = 1.2) +  # Linear model with confidence interval
+  labs(
+    title = "Relationship Between Influenza A and RSV RNA",
+    x = "Influenza A RNA (gc/g dry weight)",
+    y = "RSV RNA (gc/g dry weight)"
+  ) +
+  ggpubr::stat_cor(label.x=1e5,label.y=2.5e5,size=5, #put R2 and P-value at x,y location
+                   aes(label = paste(after_stat(rr.label), after_stat(p.label), sep = "~`,`~"))) +
+  ggpubr::stat_regline_equation(label.x=1e5,label.y=2.2e5,size=5) + #put equation at x,y location 
+  theme_minimal() +  
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 10) +
+      
+    
+  )
+
+rsv_inf_R2
+
+# Save as high-resolution PNG
+ggsave("output/Virus_RNA_Relationship_R2.png", rsv_inf, width = 8, height = 6, dpi = 300)
+
+# M. Load in the dataset from the last lecture (NSFRoD2022_Deployment_Summary_RSB.csv). 
+df <- read_csv(here("data/Lectures/NSFRoD2022_Deployment_Summary_RSB.csv"))
+
+# Let’s repeat the exercise that we did in class – 
+# to see if size increases with age and sex in elephant seals – 
+# but this time, use Standard Length (cm) instead of mass. 
+# Run an additive multiple regression of Standard Length with Age and Sex as predictors. 
+# Then, run an interactive multiple regression of Standard Length with Age and Sex as predictors. 
+# Write R code that produces/prints the intercepts and slopes of each model 
+# (feel free to either fill in a single number, or include multiple numbers in each cell to show us how you calculated e.g., intercept for males in the interactive model). 
+
 # Additive model
-# Slope for Males
-# Slope for Females
-# Intercept for Males
-# Intercept for Females
+m2 <- lm(StandardLength ~ Age + Sex, data = df)
+summary(m2)
+
+# Residuals:
+#   Min      1Q  Median      3Q     Max 
+# -23.481  -5.009   1.019   4.969  15.306 
+# 
+# Coefficients:
+#               Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)    143.744      2.961  48.548   <2e-16 ***
+#   Age           31.213      1.359  22.975   <2e-16 ***
+#   SexM           6.313      3.038   2.078   0.0467 *  
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 8.592 on 29 degrees of freedom
+# Multiple R-squared:  0.9483,	Adjusted R-squared:  0.9448 
+# F-statistic: 266.1 on 2 and 29 DF,  p-value: < 2.2e-16
+
+# Slope for Males: 31.213, both sexes parallel in slope
+# Slope for Females: 31.213
+# Intercept for Males: 150.057, males are bigger from the get go
+# Intercept for Females: 143.744
 # 
 # Interactive model
-# Slope for Males
-# Slope for Females
-# Intercept for Males
-# Intercept for Females
+m2 <- lm(StandardLength ~ Age * Sex, data = df)
+summary(m2)
+
+# Residuals:
+#   Min      1Q  Median      3Q     Max 
+# -23.825  -4.850   0.675   5.731  14.275 
+# 
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)  144.775      3.642  39.754  < 2e-16 ***
+#   Age           30.525      1.947  15.681 2.14e-15 ***
+#   SexM           4.250      5.150   0.825    0.416    
+# Age:SexM       1.375      2.753   0.499    0.621    
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 8.705 on 28 degrees of freedom
+# Multiple R-squared:  0.9488,	Adjusted R-squared:  0.9433 
+# F-statistic: 172.9 on 3 and 28 DF,  p-value: < 2.2e-16
+
+# Slope for Males: 31.9
+# Slope for Females: 30.525
+# Intercept for Males: 149.025
+# Intercept for Females: 144.775
 # 
 # 
-# N. Make a ggplot of Standard Length ~ Age with color=Sex. Manually add two best fit lines (one for males and one for females) FROM THE ADDITIVE MODEL using the slopes and intercepts in the table (hint: use geom_abline() with inputs slope=…, intercept=…, color=…).  
+# N. Make a ggplot of Standard Length ~ Age with color=Sex. Manually add two best fit lines (one for males and one for females) 
+# FROM THE ADDITIVE MODEL using the slopes and intercepts in the table (hint: use geom_abline() with inputs slope=…, intercept=…, color=…).  
 # 
 # O. Make a ggplot of Standard Length ~ Age with color=Sex. Manually add two best fit lines (one for males and one for females) FROM THE INTERACTIVE MODEL using the slopes and intercepts in the table (hint: use geom_abline() with inputs slope=…, intercept=…, color=…).  
 # P. Make a ggplot of Standard Length ~ Age with color=Sex. Add best fit lines by adding geom_smooth(method=”lm”) to the plot. Does this plot look the same as your additive model plot or your interactive model plot? 
